76a101afa613b4a7b8356d2d2657240fa4098d6a
maazk9119 <maazk9119@gmail.com>
2022-07-09 21:28:38 +0500

    implement repository pattern

---
+++ src/Rakaposhi.Core/Rakaposhi.Business.Core/BaseRepository/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rakaposhi.Business.Core.BaseRepository
+{
+    internal interface IGenericRepository<T> where T : class
+    {
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        T Find(long Id);
+
+        IEnumerable<T> GetAll();
+
+    }
+}
---
+++ src/Rakaposhi.Core/Rakaposhi.Business.Core/BaseRepository/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Rakaposhi.Business.Core.DataObjects;
+
+namespace Rakaposhi.Business.Core.BaseRepository
+{
+    internal interface IUserRepository : IGenericRepository<User>
+    {
+
+    }
+}
