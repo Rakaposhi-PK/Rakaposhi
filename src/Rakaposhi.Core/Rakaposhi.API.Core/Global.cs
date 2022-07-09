e13e1a8edab7634670337654858c22a3ddc23ad6
maazk9119 <maazk9119@gmail.com>
2022-07-10 00:29:53 +0500

    add Global variables

---
+++ src/Rakaposhi.Core/Rakaposhi.API.Core/Global.cs
@@ -0,0 +1,9 @@
+﻿namespace Rakaposhi.API.Core
+{
+    public static class Global
+    {
+        public const string APIVERSION = "v1.0";
+
+        public const string APICONTROLLER = $"api/{APIVERSION}/[controller]";
+    }
+}
