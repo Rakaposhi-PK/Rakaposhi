1538dabdc151c38840e64f5a4b5c82f76a6c4b02
maazk9119 <maazk9119@gmail.com>
2022-07-09 21:29:25 +0500

    Implement User Service

---
+++ src/Rakaposhi.Core/Rakaposhi.Business.Core/Services/IService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rakaposhi.Business.Core.Services
+{
+    //injecting Automatically to framework
+    internal interface IService
+    {
+    }
+}
