51e7ba777e48c098c3379f48969828caf9b4d553
maazk9119 <maazk9119@gmail.com>
2022-07-09 21:29:12 +0500

    Implement UserDB Class

---
+++ src/Rakaposhi.Core/Rakaposhi.Business.Core/DBRepository/DBRepositoryFactory.cs
@@ -0,0 +1,23 @@
+﻿using Rakaposhi.Business.Core.BaseRepository;
+
+namespace Rakaposhi.Business.Core.DBRepository
+{
+    internal class DBRepositoryFactory : IRepositoryFactory
+    {
+        private IUserRepository? _userRepository;
+
+
+        public IUserRepository UserRepository
+        {
+            get
+            {
+                if(_userRepository is null)
+                {
+                    _userRepository = new UserRepository();
+                }
+
+                return _userRepository;
+            }
+        }
+    }
+}
---
+++ src/Rakaposhi.Core/Rakaposhi.Business.Core/DBRepository/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using Rakaposhi.Business.Core.BaseRepository;
+using Rakaposhi.Business.Core.DataObjects;
+
+namespace Rakaposhi.Business.Core.DBRepository
+{
+    internal class UserRepository : IUserRepository
+    {
+        public void Add(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Find(long Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
