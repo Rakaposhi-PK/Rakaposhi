44d256f11927468d932472c7bc741ab1845d70fa
maazk9119 <maazk9119@gmail.com>
2022-07-10 00:29:33 +0500

    add user controller

---
+++ src/Rakaposhi.Core/Rakaposhi.API.Core/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Rakaposhi.Business.Core.DataObjects;
+using Rakaposhi.Business.Core.Services;
+
+namespace Rakaposhi.API.Core.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        private UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET: UserController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        // GET: UserController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // GET: UserController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Find(int Id)
+        {
+            return View();
+        }
+
+        public ActionResult<User> GetAll()
+        {
+            return View();
+        }
+
+    }
+}
