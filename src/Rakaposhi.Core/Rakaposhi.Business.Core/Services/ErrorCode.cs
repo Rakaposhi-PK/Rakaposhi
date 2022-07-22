3fa6b7e51a40bad369fd4a67cd61ff6547475573
maazk9119 <maazk9119@gmail.com>
2022-07-22 23:27:52 +0500

    add error codes

---
+++ src/Rakaposhi.Core/Rakaposhi.Business.Core/Services/ErrorCode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rakaposhi.Business.Core.Services
+{
+    public class ErrorCode
+    {
+        public const string ADDERROR = "ID can't be null, Please Insert ID";
+        public const string UPDATEERROR = "Unable to update record, Record doesn't exist.";
+        public const string DELETEERROR = "Unable to delete record, Record doesn't exist.";
+    }
+}
