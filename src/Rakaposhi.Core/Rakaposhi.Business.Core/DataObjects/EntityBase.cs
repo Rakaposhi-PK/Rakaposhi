b878463bda3926e4f2cbc537a1e74651b68d56cf
maazk9119 <maazk9119@gmail.com>
2022-07-22 16:06:44 +0500

    implement EntityBase

---
+++ src/Rakaposhi.Core/Rakaposhi.Business.Core/DataObjects/EntityBase.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Reflection;
+
+namespace Rakaposhi.Business.Core.DataObjects
+{
+    public abstract class EntityBase : IEntity
+    {
+        public abstract long Key { get; /*private protected set;*/ }
+
+        public void Copy(IEntity entity)
+        {
+            var type = this.GetType();
+            Copy(type, this, entity);
+        }
+
+
+        private void Copy(Type type, object source, object target)
+        {
+            var properties = type.GetProperties();
+
+            foreach (var p in properties)
+            {
+                if (p.CanWrite)
+                {
+                    p.SetValue(source, p.GetValue(target));
+                }
+            }
+        }
+    }
+}
