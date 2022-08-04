e9239b98f1825886bd242d0d6590a458ef1a4016
Maaz Khan <50416009+maazk9119@users.noreply.github.com>
2022-08-04 15:30:35 +0500

    Update run-script.sh

--- database/run-script.sh
+++ database/run-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MyPass@1234 -d master -i setup.sql
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MyPass@1234 -d RakaposhiDB #-i combine_sps.sql
+#/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MyPass@1234 -d RakaposhiDB #-i combine_sps.sql
