e723fef5ca73d5ee18670d89f9a12ee0f0f069e8
Maaz Khan <50416009+maazk9119@users.noreply.github.com>
2022-08-06 20:32:40 +0500

    Update entrypoint.sh

--- database/entrypoint.sh
+++ database/entrypoint.sh
@@ -7,15 +7,13 @@ if [ ! -f tmp/app-initialized ]; then
   function initialize_app_database(){
      sleep 90s
 
-     #/run-script.sh & /opt/mssql/bin/sqlservr
-     /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MyPass@1234 -d master -i setup.sql
+     /run-script.sh & /opt/mssql/bin/sqlservr
+     
      touch tmp/app-initialized
   } 
   initialize_app_database &
 fi
 
 sleep 60s
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MyPass@1234 -d master -i setup.sql
-#/run-script.sh & /opt/mssql/bin/sqlservr 
-
+/run-script.sh & /opt/mssql/bin/sqlservr 
 exec "$@"
