f60c9d5c5550a72d5e5ee2e403a24af7702b9e31
Maaz Khan <50416009+maazk9119@users.noreply.github.com>
2022-08-09 00:26:53 +0500

    Update run-script.sh

--- database/run-script.sh
+++ database/run-script.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MyPass@1234 -d master -i setup.sql
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MyPass@1234 -d RakaposhiDB -i combine_sps.sql
-=======
-sleep 90s
-
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MyPass@1234 -d master -i setup.sql
-#/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MyPass@1234 -d RakaposhiDB #-i combine_sps.sql
->>>>>>> 1005b15f35e86e18695eba79174950b9ee95d734
