e1354003317eaedc993ed5ad68c728030e5161ac
MariamShaikh <mariamshaikh05@gmail.com>
2022-08-08 23:23:06 +0500

    Implemented UserStatus DO

---
+++ src/Rakaposhi.Core/Rakaposhi.Business.Core/DataObjects/UserStatus.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rakaposhi.Business.Core.DataObjects
+{
+    public class UserStatus : EntityBase
+    {
+        [Required]
+        public long? RecId { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
+        public override long Key  { get => this.RecId.Value; }
+    }
+}
