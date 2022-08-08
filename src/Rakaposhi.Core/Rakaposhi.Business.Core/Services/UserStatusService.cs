bf0d9c47a3c11c404fd0cbf918045663700fb0f1
MariamShaikh <mariamshaikh05@gmail.com>
2022-08-08 23:22:23 +0500

    implented UserStatus CRUD Services

---
+++ src/Rakaposhi.Core/Rakaposhi.Business.Core/Services/UserStatusService.cs
@@ -0,0 +1,50 @@
+﻿using Rakaposhi.Business.Core.BaseRepository;
+using Rakaposhi.Business.Core.DataObjects;
+
+
+namespace Rakaposhi.Business.Core.Services
+{
+    public class UserStatusService: IService
+    {
+        private IRepositoryFactory _factory;
+        public UserStatusService(IRepositoryFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public void Add(UserStatus userStatus)
+        {
+            if(userStatus.RecId == null)
+            {
+                throw new ServiceException(ErrorCode.ADDERROR);
+            }
+
+            _factory.UserStatusRepository.Add(userStatus);
+           
+        }
+        public void Update(UserStatus userStatus)
+        {
+            var found = _factory.UserStatusRepository.Find(userStatus.RecId.Value);
+
+            _factory.UserStatusRepository.Update(found);
+
+            //Console.WriteLine("Updated");
+
+        }
+        public void Delete(long id)
+        {
+            var found = _factory.UserStatusRepository.Find(id);
+
+            _factory.UserStatusRepository.Delete(found);
+        }
+        public UserStatus Find(long id)
+        {
+            return _factory.UserStatusRepository.Find(id);
+        }
+
+        //public IEnumerable<UserStatus> GetAll()
+        //{
+
+        //}
+    }
+}
