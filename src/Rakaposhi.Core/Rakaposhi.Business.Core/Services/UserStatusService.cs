78114c3ca20c27aa8d30e99802e09921d4897ab9
MariamShaikh <mariamshaikh05@gmail.com>
2022-08-09 22:56:02 +0500

    Implemented UserStatus CRUD Services

--- src/Rakaposhi.Core/Rakaposhi.Business.Core/Services/UserStatusService.cs
+++ src/Rakaposhi.Core/Rakaposhi.Business.Core/Services/UserStatusService.cs
@@ -25,15 +25,17 @@ namespace Rakaposhi.Business.Core.Services
         public void Update(UserStatus userStatus)
         {
             var found = _factory.UserStatusRepository.Find(userStatus.RecId.Value);
+            if(found == null)
+            {
+                throw new ServiceException(ErrorCode.UPDATEERROR);
+            }
 
-            _factory.UserStatusRepository.Update(found);
-
-            //Console.WriteLine("Updated");
-
+            _factory.UserStatusRepository.Update(userStatus);
         }
         public void Delete(long id)
         {
             var found = _factory.UserStatusRepository.Find(id);
+
 
             _factory.UserStatusRepository.Delete(found);
         }
@@ -42,9 +44,9 @@ namespace Rakaposhi.Business.Core.Services
             return _factory.UserStatusRepository.Find(id);
         }
 
-        //public IEnumerable<UserStatus> GetAll()
-        //{
-
-        //}
+        public IEnumerable<UserStatus> GetAll()
+        {
+           return _factory.UserStatusRepository.GetAll();
+        }
     }
 }
