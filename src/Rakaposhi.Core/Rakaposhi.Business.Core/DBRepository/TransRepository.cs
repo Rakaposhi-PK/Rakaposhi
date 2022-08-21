b81843e2cfa278616d3d2fa1fb4a28ca89884bbb
MariamShaikh <mariamshaikh05@gmail.com>
2022-08-21 20:52:44 +0500

    Implemented Transactions DB Repository

---
+++ src/Rakaposhi.Core/Rakaposhi.Business.Core/DBRepository/TransRepository.cs
@@ -0,0 +1,38 @@
+﻿using Rakaposhi.Business.Core.BaseRepository;
+using Rakaposhi.Business.Core.DataObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rakaposhi.Business.Core.DBRepository
+{
+    public class TransRepository : ITransRepository
+    {
+        public void Add(Trans entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Trans entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Trans Find(long Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Trans> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Trans entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
